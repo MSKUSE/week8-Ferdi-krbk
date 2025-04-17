a9f5bdc4a7e1ca359ed529c6a4d6948289ad5e68
Ferdi-krbk <ferdiderebasi@posta.mu.edu.tr>
2025-04-17 13:16:05 +0300

    Week 8

---
+++ src/Shape.java
@@ -0,0 +1,40 @@
+public class Shape {
+
+    private String color = "white";
+
+    public Shape(){
+
+    }
+    public Shape(String color) {
+        this.color = color;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public double area(){
+        return 0.0;
+    }
+    public double perimeter(){
+        return 0.0;
+    }
+
+    public void describe(){
+        System.out.println("This is a shape and color is " + this.color);
+    }
+    /*
+    public void describe(int sideA, int sideB){
+        System.out.println("Side A is " + sideA + " Side B is " + sideB);
+    }
+
+    public void describe(int sideA){
+        System.out.println("Side A is " + sideA);
+    }
+    public void describe(double sideA) {
+        System.out.println("Radius is" + sideA);
+    }*/
+}
